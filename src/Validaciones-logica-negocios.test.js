72f988c9f2a2151b21f3e3fd919255b10d578d89
Jav4nsJuice <javans.ferrel@gmail.com>
2022-10-12 11:06:27 -0400

    Empty Title Fields

---
+++ src/Validaciones-logica-negocios.test.js
@@ -0,0 +1,16 @@
+import fs from "fs";
+import * as gestor from './Tarea-logica-negocios.js';
+import * as listas from './Listas-logica-negocios.js';
+import * as validaciones from './Validaciones-logica-negocios.js';
+import { Tarea } from './Tarea-logica-negocios.js';
+import { isTypedArray } from "util/types";
+
+describe("Gestor Tareas Validaciones", () => {
+    it("Debería devolver n/a cuando un campo se envía vacío", () => {
+        expect(validaciones.verificarCampoVacio("")).toEqual("n/a");
+    });
+
+    it("Debería devolver n/a cuando un campo se envía vacío pero con espacios", () => {
+        expect(validaciones.verificarCampoVacio(" ")).toEqual("n/a");
+    });
+});
